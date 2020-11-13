512151478eb02c8f39c93ba767c7a2e521553227
8059586 <70647848+8059586@users.noreply.github.com>
2020-11-13 17:56:08 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Super-Sonic-Racing-Build-2
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ js/Game.js
@@ -0,0 +1,50 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  start(){
+      player = new Player();
+      player.getCount();
+      form = new Form()
+      form.display();
+  }
+
+  play(){
+    form.hide();
+    textSize(30);
+    text("Game Start", 120, 100)
+    Player.getPlayerInfo();
+
+    if(allPlayers !== undefined){
+      var anyvariablename = 130;
+      for(var i in allPlayers){
+        if (i === "player" + player.index){
+          fill("red")}
+        else{
+          fill("black");
+        }
+        anyvariablename+=20;
+        textSize(15);
+        text(allPlayers[i].name + ": " + allPlayers[i].distance, 120,anyvariablename)
+      }
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=50
+      player.update();
+    }
+  }
+}
